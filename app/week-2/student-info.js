a5d9df8c96b91cfe5105e88c6cdb0ab52f9b318d
Cale Ryden <cale.ryden@edu.sait.ca>
2025-09-11 09:58:23 -0600

    Update student-info.js

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -3,6 +3,7 @@ import Link from 'next/link'
 export default function StudentInfo() {
  return (
  <main>
+ <h1>Shopping List</h1>
  <p>Cale Ryden</p>
  <Link href="https://github.com/cryden05/cprg306-assignments.git">Github Repository</Link>
  </main>
