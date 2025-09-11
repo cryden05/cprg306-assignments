d37f9fc072c28ec9fba76150c52ddff873cc20ce
Cale Ryden <cale.ryden@edu.sait.ca>
2025-09-11 09:57:17 -0600

    Initial Commit

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,10 +1,10 @@
 import Link from 'next/link'
+import StudentInfo from "./student-info"
 
 export default function Page() {
  return (
  <main>
- <h1>CPRG 306:Web Development 2 - Assignments</h1>
-  <Link href="week-2\">Week 2</Link>
+  <StudentInfo />
  </main>
  );
 }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function StudentInfo() {
+ return (
+ <main>
+ <p>Cale Ryden</p>
+ <Link href="https://github.com/cryden05/cprg306-assignments.git">Github Repository</Link>
+ </main>
+ );
+}
