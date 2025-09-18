f01b941ee6262e7f4cb383f688bd413d5c26485d
Cale Ryden <cale.ryden@edu.sait.ca>
2025-09-18 10:30:19 -0600

    Changes to list

--- app/week-3/item-list.js
+++ app/week-3/item-list.js
@@ -75,18 +75,18 @@ const item12 = {
 export default function ItemList(){
     return (
         <ul>
-            <Item name={item1.name} quantity={item1.quantity} category={item1.category} />
-            <Item name={item2.name} quantity={item2.quantity} category={item2.category} />
-            <Item name={item3.name} quantity={item3.quantity} category={item3.category} />
-            <Item name={item4.name} quantity={item4.quantity} category={item4.category} />
-            <Item name={item5.name} quantity={item5.quantity} category={item5.category} />
-            <Item name={item6.name} quantity={item6.quantity} category={item6.category} />
-            <Item name={item7.name} quantity={item7.quantity} category={item7.category} />
-            <Item name={item8.name} quantity={item8.quantity} category={item8.category} />
-            <Item name={item9.name} quantity={item9.quantity} category={item9.category} />
-            <Item name={item10.name} quantity={item10.quantity} category={item10.category} />
-            <Item name={item11.name} quantity={item11.quantity} category={item11.category} />
-            <Item name={item12.name} quantity={item12.quantity} category={item12.category} />
-        </ul>
+            <Item props={item1} />
+            <Item props={item2} />
+            <Item props={item3} />
+            <Item props={item4} />
+            <Item props={item5} />
+            <Item props={item6} />
+            <Item props={item7} />
+            <Item props={item8} />
+            <Item props={item9} />
+            <Item props={item10} />
+            <Item props={item11} />
+            <Item props={item12} />
+        </ul>    
     )
 }
