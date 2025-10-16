d7b59cf860bada1a2803bec9b89ee9d2273898ca
Cale Ryden <cale.ryden@edu.sait.ca>
2025-10-16 15:43:21 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Page() {
  return (
- <main className="bg-black">
+ <main className="bg-black text-white">
  <h1>CPRG 306:Web Development 2 - Assignments</h1>
   <Link href="week-2">Week 2</Link> <br />
   <Link href="week-3">Week 3</Link> <br />
